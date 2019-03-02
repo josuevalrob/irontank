7098aa92fbedbed63a33a07b66be099f9841834f
Josue Valencia <josue.valencia@domo.ec>
2019-03-02 15:55:06 +0100

    canvas first version ready

--- js/canvas.js
+++ js/canvas.js
@@ -1,18 +1,23 @@
-export let canvas = {  
+import {updateGame} from './main.js';
+let canvas = {  
     image : new Image(),  
     canvas : document.createElement('canvas'), 
-    frames: 320,
-    pause: false,
     start : function () {
-      this.context = this.canvas.getContext('2d')
-      this.canvas.width = 880;
-      this.canvas.height = 470;
-      this.image.src = './images/bg.png';       
-      this.image.onload = updateGame();  
-  
-      document.getElementById('game-board').appendChild(this.canvas);
+        this.context = this.canvas.getContext('2d')
+        this.canvas.width = 964; //964x904 pixels
+        this.canvas.height = 865;
+        this.image.src = '../styles/assets/images/27982.png';       
+        this.image.onload = updateGame();  
+        document.getElementById('game-board').appendChild(this.canvas);
     },  
     stop: function(){
-      this.pause = !this.pause;
-    }   
-  }
+        this.pause = !this.pause;
+    }, 
+    bkgDraw :  function (){
+        this.context.drawImage(this.image, 0, 0);
+    } 
+}
+
+
+
+export {canvas}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+import {canvas} from './canvas.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   // we will create the canvas onece it is loaded. 
+  startGame();
 })
+
+function startGame(){
+  canvas.start();
+}
+
+
+function updateGame (){
+  canvas.bkgDraw()
+  requestAnimationFrame(updateGame);      
+}
+
+export {updateGame};
