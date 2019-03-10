78d7724f2a36f2afa159ccb58a9abd0878fae27b
Josue Valencia <josue.valencia@domo.ec>
2019-03-10 15:53:09 +0100

    map design

--- js/helpers/positions.js
+++ js/helpers/positions.js
@@ -3,40 +3,15 @@ let hor = (window.innerWidth / 2) - 50
 let ver = (window.innerHeight / 2) - 50
 let positions = {
   random: [
-    [630, 500],
+    [830, 500],
     [100, 400],
     [200, 200],
     [350, 400],
     [400, 400],
     [500, 200],
-    [650, 400],
-    [700, 500],
-  ], 
-  square : [
-    [hor, ver], 
-    [hor + 50, ver],
-    [hor + 50, ver + 50],
-    [hor, ver + 50],
-  ],
-  cross: [
-    [hor, ver], 
-    [hor + 50, ver],
-    [hor + 50, ver + 50],
-    [hor, ver + 50],
-    // line
-    [hor - 50, ver],
-    [hor - 100, ver],
-    [hor - 150, ver],
-    [hor - 200, ver],
-  ],
-  line : [
-    [50, 200],
-    [100, 200],
-    [150, 200],
-    [200, 200],
-    [250, 200],
-    [300, 200]
-  ], 
+    [650, 300],
+    [700, 300],
+  ]  
 }
 
 export {positions}
--- js/helpers/randomMap.js
+++ js/helpers/randomMap.js
@@ -7,10 +7,60 @@ let objects = {}
 let objectsKeys
 let hor = window.innerWidth
 let ver = (window.innerHeight / 2) - 50
-let horTotal = Math.floor(hor / 50)
-let horMid = Math.floor(horTotal/2)
+// let horMid = Math.floor(horTotal/2)
+function caltot(e){
+  return Math.floor(e / 50)
+}
 let increase = 0
 function randomMap () { 
+  
+  objects.blocks = 
+          Array.from(Array(caltot(ver)), (_,i)=>{
+            increase = i * 50            
+            return new woodBlock((hor / 2) + 30, 200  - increase + 35)
+          }
+        ).concat( 
+          Array.from(Array(caltot(hor)), (_,i)=>{
+            increase = i * 50
+            return new ironBlock(hor - 60 - increase, ver)
+            })
+        ).concat(
+          Array.from(Array(caltot(hor)), (_,i)=>{
+            increase = i * 50
+            return new woodBlock(hor - 60 - increase, ver + 50)
+          })
+        ).concat(
+          Array.from(Array(caltot(hor)), (_,i)=>{
+            increase = i * 50
+            return new woodBlock(hor - 60 - increase, ver - 50)
+          })
+        ).concat(
+          Array.from(Array(caltot(ver)), (_,i)=>{
+            increase = i * 50
+            return new woodBlock((hor / 2) - 70, 200  - increase + 35)
+          })
+        ).concat(
+          Array.from(Array(caltot(ver)), (_,i)=>{
+            increase = i * 50
+            return new ironBlock((hor / 2) - 20, 200  - increase + 35)
+          })
+        ).concat(
+          Array.from(Array(caltot(ver)), (_,i)=>{
+            increase = i * 50
+            return new ironBlock((hor / 2) - 20, (ver * 1.935)  - increase + 35)
+          })
+        ).concat(
+          Array.from(Array(caltot(ver)), (_,i)=>{
+            increase = i * 50
+            return new woodBlock((hor / 2) + 30, (ver * 1.935)  - increase + 35)
+          })
+        ).concat(
+          Array.from(Array(caltot(ver)), (_,i)=>{
+            increase = i * 50
+            return new woodBlock((hor / 2) - 70, (ver * 1.935)  - increase + 35)
+          })
+        )
+// Trees
   shuffleArry(positions.random) 
   objects.trees = positions.random.map((e, i) =>{
     if(i%2 == 0){
@@ -18,21 +68,29 @@ function randomMap () {
     } else {
       return new Tree(35, 35, 'treesmall.png', e[0], e[1])
     }
-  })
-
-  objects.blocks = positions.line.map(e => {
-        return new woodBlock(e[0], e[1])
-      }).concat( 
-        Array.from(Array(horMid), (_,i)=>{
-          increase = i * 50
-          return new ironBlock(hor - increase, ver)
-          })
-      ).concat(
-        Array.from(Array(horMid), (_,i)=>{
-          increase = i * 50
-          return new ironBlock(hor - increase, ver + 50)
-        })
-      )
+  }).concat(
+    Array.from(Array(caltot(ver)), (_,i)=>{
+      increase = i * 50
+      let x = (hor / 2) - 120
+      let y = (ver * 1.935)  - increase + 35
+      if(i%2 == 0){
+        return new Tree(50, 50, 'tree.png', x, y)
+      } else {
+        return new Tree(35, 35, 'treesmall.png', x, y)
+      }
+    })
+  ).concat(
+    Array.from(Array((caltot(ver) - 1)), (_,i)=>{
+      increase = i * 50
+      let x = (hor / 2) + 80
+      let y = 5 + increase + 35
+      if(i%2 == 0){
+        return new Tree(50, 50, 'tree.png', x, y)
+      } else {
+        return new Tree(35, 35, 'treesmall.png', x, y)
+      }
+    })
+  )
 
   objectsKeys = Object.keys(objects);
 }
